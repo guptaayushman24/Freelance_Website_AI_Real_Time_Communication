aa2e7c0a91b58fd04aeda06394488cd08cd7a265
Ayushman Gupta <guptaayushman24@gmail.com>
2025-05-18 22:32:01 +0530

    Vercel Deployed

--- freelance_ai/next.config.ts
+++ freelance_ai/next.config.ts
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import {PrismaPlugin} from '@prisma/nextjs-monorepo-workaround-plugin'
 const nextConfig: import('next').NextConfig = {
   // Other configurations
   eslint:{
@@ -7,12 +6,6 @@ const nextConfig: import('next').NextConfig = {
   },
   typescript:{
     ignoreBuildErrors:true
-  },
-   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-    return config
   },
 }
 
