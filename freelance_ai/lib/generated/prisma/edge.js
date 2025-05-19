b5742ede800971662e453f6922606e9149a22764
Ayushman Gupta <guptaayushman24@gmail.com>
2025-05-19 12:55:50 +0530

    generate command

--- freelance_ai/lib/generated/prisma/edge.js
+++ freelance_ai/lib/generated/prisma/edge.js
@@ -234,8 +234,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.8.2",
--- freelance_ai/lib/generated/prisma/index.js
+++ freelance_ai/lib/generated/prisma/index.js
@@ -235,8 +235,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.8.2",
@@ -264,8 +263,8 @@ const fs = require('fs')
 config.dirname = __dirname
 if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
   const alternativePaths = [
+    "../lib/generated/prisma",
     "lib/generated/prisma",
-    "generated/prisma",
   ]
   
   const alternativePath = alternativePaths.find((altPath) => {
@@ -295,7 +294,7 @@ Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
 path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "lib/generated/prisma/query_engine-windows.dll.node")
+path.join(process.cwd(), "../lib/generated/prisma/query_engine-windows.dll.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
-path.join(process.cwd(), "lib/generated/prisma/schema.prisma")
+path.join(process.cwd(), "../lib/generated/prisma/schema.prisma")
