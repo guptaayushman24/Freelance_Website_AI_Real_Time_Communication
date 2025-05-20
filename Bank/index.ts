deb5c22a4f92bb202f0380d69322c2f7573f2f9a
Ayushman Gupta <guptaayushman24@gmail.com>
2025-05-20 13:29:29 +0530

    index.ts file change

--- Bank/index.ts
+++ Bank/index.ts
@@ -1,4 +1,5 @@
-import {PrismaClient} from '../freelance_ai/node_modules/.prisma/client'
+// import {PrismaClient} from '../freelance_ai/node_modules/.prisma/client'
+import { PrismaClient } from "@prisma/client";
 const express = require('express');
 const cors = require('cors');
 const app = express();
