2ffca81754f404dfc1323142a36e4957cad96ed8
Ayushman Gupta <guptaayushman24@gmail.com>
2025-05-20 10:58:54 +0530

    lib prisma.ts file change

--- freelance_ai/lib/prisma.ts
+++ freelance_ai/lib/prisma.ts
@@ -3,7 +3,7 @@
 // export const prisma = globalForPrisma.prisma || new PrismaClient()
 // if (process.env.NODE_ENV!=='production') globalForPrisma.prisma = prisma
 
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
